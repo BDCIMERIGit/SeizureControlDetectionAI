be428df9f832fc8ba50525f5cc9c8def47e30111
BDCIMERIGit <bdc.imeri-idealab@ui.ac.id>
2025-10-28 14:44:19 +0700

    AplikasiDrRafli3Model.py

--- AplikasiDrRafli3Model.py
+++ AplikasiDrRafli3Model.py
@@ -9,7 +9,7 @@ import pandas as pd
 import os
 from collections import Counter
 
-st.set_page_config(page_title="🧠 Selamat Datang di SeizureDetect.AI!", layout="centered")
+st.set_page_config(page_title="🧠 Halo! 🧠", layout="centered")
 
 # =====================================================
 # 🎨 Custom CSS Styling
@@ -203,8 +203,8 @@ PAGES = ["home", "auth_choice", "register", "login", "form", "history"]
 # 5️⃣ UI Halaman
 # =====================================================
 if st.session_state["page"] == "home":
-    st.title("🧠 Selamat Datang di SeizureDetect.AI!")
-    st.markdown("### Prediksi Pengendalian Kejang Menggunakan Ensemble Model")
+    st.title("🧠 Halo! 🧠")
+    st.markdown("### Selamat Datang di Aplikasi SeizureDetect.AI!")
     if st.button("Mulai Aplikasi"):
         go_to("auth_choice")
 
@@ -331,6 +331,7 @@ elif st.session_state["page"] == "history":
 
 st.markdown("---")
 st.caption("Developed with ❤️ by Dr. Rafli, AISeeyou, & BDC IMERI | Ensemble Epilepsy Prediction Model (XGB + DT + RF)")
+
 
 
 
