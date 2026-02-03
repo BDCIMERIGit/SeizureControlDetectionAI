b1939e5b68988c5b0ed8c121f8d7d02990d59874
BDCIMERIGit <bdc.imeri-idealab@ui.ac.id>
2026-02-03 23:39:11 +0700

    Update AplikasiDrRafli3Model.py

--- AplikasiDrRafli3Model.py
+++ AplikasiDrRafli3Model.py
@@ -128,15 +128,6 @@ div[data-testid="stFormSubmitButton"] > button:hover {
         font-weight: 700 !important;
     }
 
-    /* ====== LOGO HEADER ======= */
-    .logo-header {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 6px;              /* 🔥 jarak logo DI SINI */
-    padding: 10px 20px;
-}
-
 
     /* ====== SIDEBAR ====== */
     section[data-testid="stSidebar"] {
@@ -245,6 +236,15 @@ div[data-testid="stFormSubmitButton"] > button:hover {
         font-weight: 800;
         color: #001f3f;
     }
+
+        /* ====== HEADER LOGO ====== */
+    .logo-header {
+        display: flex;
+        align-items: center;
+        gap: 6px;              /* 🔥 jarak antar logo */
+        padding: 8px 20px;
+    }
+
 
     </style>
 """, unsafe_allow_html=True)
@@ -900,6 +900,7 @@ elif st.session_state["page"] == "history":
 if st.session_state.get("page", "") != "home":
     st.markdown("---")
     st.caption("Developed with ❤️ by Dr. Rafli, AISeeyou, & BDC IMERI | Ensemble Epilepsy Prediction Model (XGB + DT + RF)")
+
 
 
 
