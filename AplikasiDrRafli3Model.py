b16967dc57ba04c11feb4481f8cdd5faf936c7cd
BDCIMERIGit <bdc.imeri-idealab@ui.ac.id>
2026-02-04 00:02:09 +0700

    Update AplikasiDrRafli3Model.py

--- AplikasiDrRafli3Model.py
+++ AplikasiDrRafli3Model.py
@@ -19,12 +19,20 @@ st.set_page_config(page_title="Halo Sahabat!", layout="centered")
 # =====================================================
 # 🖼️ Header Logo Section
 # =====================================================
-col1, col2 = st.columns([1,1])
+#col1, col2 = st.columns([1,1])
+#with col1:
+#    st.image("logo/logo-ui-fk-imeri.png", width=200)
+#with col2:
+#    st.image("logo/logo-RSCM.png", width=200)
+
+col1, col2, col3 = st.columns([5, 0.3, 5])
+
 with col1:
     st.image("logo/logo-ui-fk-imeri.png", width=200)
 with col2:
+    st.write("")  # spacer kecil
+with col3:
     st.image("logo/logo-RSCM.png", width=200)
-
 
 #st.markdown("""
 #<div class="logo-header">
@@ -891,6 +899,7 @@ elif st.session_state["page"] == "history":
 if st.session_state.get("page", "") != "home":
     st.markdown("---")
     st.caption("Developed with ❤️ by Dr. Rafli, AISeeyou, & BDC IMERI | Ensemble Epilepsy Prediction Model (XGB + DT + RF)")
+
 
 
 
